20bdb3a005efead32018bf20f312e1d20bafca66
upupzealot <hzxjhspxn@163.com>
2014-12-12 22:13:32 +0800

    Fix last presented bug
    Changed the way to rerendering the whole map when flipping, Now it works
    nicely even in UC and Chrome

--- Map.js
+++ Map.js
@@ -10,6 +10,20 @@ function Map() {
 	Bitmap.load("Slate.png");
 	Bitmap.load("Tile.png");
 
+	this.tiles = new Array();
+	for(var i = 0; i < 5; i++) {
+		this.tiles[i] = new Array();
+		for(var j = 0; j < 8; j++) {
+			if(Math.random() * 100 < 10) {
+				this.tiles[i][j] = "Bush_Small";
+			} else if(Math.random() * 100 < 5) {
+				this.tiles[i][j] = "Slate";
+			} else {
+				this.tiles[i][j] = "Tile";
+			}
+		}
+	}
+
 	this.hero = new Hero();
 	this.hero.x = 2 * 80 + 40;
 	this.hero.y = 6 * 80 + 10;
@@ -20,25 +34,30 @@ function Map() {
 Map.prototype.start = function() {
 	var self = this.self;
 
-	self.buffer_context.save();
-	self.buffer_context.translate(80, 80);
+	var current_time = new Date().getTime();
+	self.render_map();
+	Screen.flip_cost = new Date().getTime() - current_time;
+
+}
+
+Map.prototype.render_map = function() {
+	var self = this.self;
+
 	for(var i = 0; i < 5; i++) {
 		for(var j = 0; j < 8; j++) {
-			var file_name = null;
-			if(Math.random() * 100 < 10) {
-				self.buffer_context.drawImage(Bitmap.pool["Bush_Small.png"], i * 80, j * 80);
-			} else if(Math.random() * 100 < 5) {
-				self.buffer_context.drawImage(Bitmap.pool["Slate.png"], i * 80, j * 80);
-			} else {
-				self.buffer_context.drawImage(Bitmap.pool["Tile.png"], i * 80, j * 80);
-			}
+			self.buffer_context.drawImage(Bitmap.pool[self.tiles[i][j] + ".png"], 80 + i * 80, 80 + j * 80);
 		}
 	}
-	self.buffer_context.restore();
 
-	self.buffer_context.drawImage(self.buffer, 80, 640, 400, 80, 80, 0, 400, 80);
-	self.buffer_context.drawImage(self.buffer, 80, 80, 80, 640, 480, 80, 80, 640);
-	self.buffer_context.drawImage(self.buffer, 400, 80, 80, 640, 0, 80, 80, 640);
+	for(var i = 0; i < 5; i++) {
+		self.buffer_context.drawImage(Bitmap.pool[self.tiles[i][7] + ".png"], 80 + i * 80, 0);
+	}
+	for(var j = 0; j < 8; j++) {
+		self.buffer_context.drawImage(Bitmap.pool[self.tiles[4][j] + ".png"], 0, 80 + j * 80);
+	}
+	for(var j = 0; j < 8; j++) {
+		self.buffer_context.drawImage(Bitmap.pool[self.tiles[0][j] + ".png"], 480, 80 + j * 80);
+	}
 }
 
 Map.prototype.flip = function(direction) {
@@ -47,18 +66,50 @@ Map.prototype.flip = function(direction) {
 	var current_time = new Date().getTime();
 	switch (direction) {
 		case "up": 
-			self.buffer_context.drawImage(self.buffer, 80, 0, self.width, self.height, 80, 80, self.width, self.height);
-			self.buffer_context.drawImage(self.buffer, 80, self.height, self.width, 80, 80, 0, self.width, 80);
+			var bottom = new Array();
+			for(var i = 0; i < 5; i++) {
+				bottom[i] = self.tiles[i][7];
+			}
+			for(var i = 0; i < 5; i++) {
+				for(var j = 7; j >= 0; j--) {
+					self.tiles[i][j] = self.tiles[i][j - 1];
+				}
+			}
+			for(var i = 0; i < 5; i++) {
+				self.tiles[i][0] = bottom[i];
+			}
+			self.render_map();
 			break;
 		case "left" : 
-			self.buffer_context.drawImage(self.buffer, 80, 0);
-			self.buffer_context.drawImage(self.buffer, 80, 0, 80, 640, 480, 0, 80, 640);
-			self.buffer_context.drawImage(self.buffer, 400, 0, 80, 640, 0, 0, 80, 640);
+			var right = new Array();
+			for(var j = 0; j < 8; j++) {
+				right[j] = self.tiles[4][j];
+			}
+			for(var i = 4; i >= 1; i--) {
+				for(var j = 0; j < 8; j++) {
+					self.tiles[i][j] = self.tiles[i - 1][j];
+				}
+			}
+			for(var j = 0; j < 8; j++) {
+				self.tiles[0][j] = right[j];
+			}
+			self.render_map();
 			break;
 		case "right" : 
-			self.buffer_context.drawImage(self.buffer, -80, 0);
-			self.buffer_context.drawImage(self.buffer, 80, 0, 80, 640, 480, 0, 80, 640);
-			self.buffer_context.drawImage(self.buffer, 400, 0, 80, 640, 0, 0, 80, 640);
+			var left = new Array();
+			for(var j = 0; j < 8; j++) {
+				left[j] = self.tiles[0][j];
+			}
+			for(var i = 0; i < 4; i++) {
+				for(var j = 0; j < 8; j++) {
+					self.tiles[i][j] = self.tiles[i + 1][j];
+				}
+			}
+			for(var j = 0; j < 8; j++) {
+				self.tiles[4][j] = left[j];
+			}
+			self.render_map();
+			break;
 			break;
 	}
 	Screen.flip_cost = new Date().getTime() - current_time;
