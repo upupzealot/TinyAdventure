08eafdf04c92e6071b2d6514a9e22c12f3bae47c
upupzealot <hzxjhspxn@163.com>
2014-12-13 17:54:10 +0800

    tween dialog
    add tween to the show and hide of the dialog window. so that the window
    wont close sunddenly in the same time it shows.

--- DialogBoard.js
+++ DialogBoard.js
@@ -4,26 +4,64 @@ function DialogBoard() {
 	//this.image = new NinePatch("Bubble.png", 24, 24, 23, 23);
 	this.image = new NinePatch("Bubble_filled.png", 24, 24, 23, 23);
 	this.active = false;
-	this.width = 180;
-	this.height = 180;
+
+	this.width_min = 46;
+	this.height_min = 48;
+
+	this.width_max = 180;
+	this.height_max = 180;
+
+	this.width = 46;
+	this.height = 48;
+
+	this.state = "hide";
+	this.tween_count = 0;
+	this.tween_interval = 0.5;
 }
 
 DialogBoard.prototype.show = function() {
 	var self = this.self;
 
 	self.active = true;
+	self.state = "showing";
 }
 
 DialogBoard.prototype.hide = function() {
 	var self = this.self;
 
-	self.active = false;
-	self.scene.hero.stopped = false;
+	self.state = "hiding";
 }
 
-//DialogBoard.prototype.update = function(dt) {
-//
-//}
+DialogBoard.prototype.update = function(dt) {
+	var self = this.self;
+
+	if(self.state == "showing") {
+		self.tween_count += dt;
+		if(self.tween_count >= self.tween_interval) {
+			self.tween_count = 0;
+			self.state = "show";
+			return;
+		}
+		var process = self.tween_count / self.tween_interval;
+		self.width = Math.easeOutBack(self.width_min, self.width_max, process);
+		self.height = Math.easeOutBack(self.height_min, self.height_max, process);
+	}
+
+	if(self.state == "hiding") {
+		self.tween_count += dt;
+		if(self.tween_count >= self.tween_interval) {
+			self.tween_count = 0;
+			self.state = "hide";
+			self.active = false;
+			self.scene.hero.stopped = false;
+			return;
+		}
+		var process = self.tween_count / self.tween_interval;
+		process = 1 - process;
+		self.width = Math.easeOutBack(self.width_min, self.width_max, process);
+		self.height = Math.easeOutBack(self.height_min, self.height_max, process);
+	}
+}
 
 DialogBoard.prototype.render = function(ctx) {
 	var self = this.self;
